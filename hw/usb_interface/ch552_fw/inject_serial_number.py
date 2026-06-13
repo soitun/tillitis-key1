ad727a6291d9d52ba6d82683db09be6a0d0a8b71
Tobias Olausson <tobias@eub.se>
2026-06-13 13:55:06 +0200

    ch552: allow printing the injected uuid
    If one wants to test that the USB firmware flashed is the one that we
    added a new serial number to, knowing what serial number was injected is
    required.

--- hw/usb_interface/ch552_fw/inject_serial_number.py
+++ hw/usb_interface/ch552_fw/inject_serial_number.py
@@ -9,12 +9,14 @@ import encode_usb_strings
 
 
 magic = encode_usb_strings.string_to_descriptor("68de5d27-e223-4874-bc76-a54d6e84068f")
-replacement = encode_usb_strings.string_to_descriptor(str(uuid.uuid4()))
+random_uuid = str(uuid.uuid4())
+replacement = encode_usb_strings.string_to_descriptor(random_uuid)
 
 
 parser = argparse.ArgumentParser(description='CH552 USB serial number injector. Replaces the default UUID with a randomly generated UUID4')
 parser.add_argument('-i', required=True, help='input file')
 parser.add_argument('-o', required=True, help='output file')
+parser.add_argument('-v', required=False, action="store_true", help='verbose, print new uuid')
 args = parser.parse_args()
 
 f = bytearray(open(args.i, 'rb').read())
@@ -30,3 +32,5 @@ f[pos:(pos+len(magic))] = replacement
 with open(args.o, 'wb') as of:
     of.write(f)
     
+if args.v:
+    print(f"{random_uuid}")
